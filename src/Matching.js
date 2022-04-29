359bb130abe9fdd4bdc0ab494fceb43a587b5cad
WhitneyBrummet <89543717+WhitneyBrummet@users.noreply.github.com>
2022-04-28 17:15:38 -0700

    adding comments

--- src/Matching.js
+++ src/Matching.js
@@ -23,9 +23,9 @@ const Matching = props => {
 		}, 
 	];
 	
-	const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false); /***need this***/
 	const mixed = arrayShuffle(FlashCard);
-	const [percent, setPercent] = useState(0);
+	const [percent, setPercent] = useState(0); /***need this***/
 	const getAnswers = mixed => {
 		let content = [];
 		for (let i = 0; i < mixed.length; i++) {
@@ -34,7 +34,11 @@ const Matching = props => {
 		}
 		return content;
 	};
+	
+	/***need this function***/
 	const togglePopup = () => {
+		
+		/***This code is to calculate percentage. This will be different for you***/
 		var correct = mixed;
 		var correctAnswers = mixed.map(function(value) {return value.cardBack;});
 		var Answer = getAnswers(correct);
@@ -42,12 +46,14 @@ const Matching = props => {
 		if (JSON.stringify(correctAnswers) === JSON.stringify(Answer)){setPercent(100);}
 		else {
 			correctAnswers.forEach(function(element, index) {if (element === Answer[index]){result.push(element);}})
+			
+			/***sets percent to display score. Might be a little different for you but its needed.***/
 			var number = (result.length/mixed.length)*100;
-			setPercent(number.toFixed(2));
+			setPercent(number.toFixed(2)); 
 		}
 		
 		
-		setIsOpen(!isOpen);
+		setIsOpen(!isOpen); /***need this***/
 	}
 
 return (
@@ -75,22 +81,24 @@ return (
           ))}
         </tbody>
       </table>
+	/***From here***/
 	  <button id="submit" onClick={togglePopup}> Submit </button>
 	  {isOpen && <PopupScore
-        content={<>
-          <h1>Score</h1>
-          <div>
+        	content={<>
+          		<h1>Score</h1>
+          		<div>
 			  <p id="percent"> {percent}% </p>
 			  <div>
 				<button id="Pop" style={{marginRight: 40}}>View Incorrect Answers</button>
 				<button id="Pop">Back to home page</button>
 			  </div>
-		  </div>
-        </>}
-        handleClose={togglePopup}
-      />}
+		  	</div>
+        	</>}
+        	handleClose={togglePopup}
+      		/>}
+	/***to here***/
 	 </div>
-)
+    )
  
 }
 

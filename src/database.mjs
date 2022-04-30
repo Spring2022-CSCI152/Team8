abcb203edd9908a4722455bf79812454f0225cf9
nwanderson009 <82849228+nwanderson009@users.noreply.github.com>
2022-04-30 15:18:01 -0700

    Revert "Revert "backend""
    This reverts commit 1cae6125d64b5fe629a27cbea2f71ea16126e8f4.

--- src/database.mjs
+++ src/database.mjs
@@ -1,23 +1,32 @@
-import { MongoClient } from 'mongodb'
-var db
-var cards
-var client
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url);
+const { MongoClient } = require('mongodb');
+var db;
+var cards;
+var users;
+var client;
 import 'dotenv/config'
 async function setUpDB() {
     // Mongo will not connect unless your IP is whitelisted
-    client = new MongoClient(`mongodb+srv://ianc:${encodeURIComponent(process.env.MONGODB_PASSWORD)}@cluster0.ptnk2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+    client = new MongoClient(`mongodb+srv://Na:${encodeURIComponent(process.env.MONGODB_PASSWORD)}@cluster0.ptnk2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
     await client.connect()
     db = client.db("myFirstDatabase")
     cards = db.collection("Cards")
-}
-function getDB() {
-    return db
-}
-function getCards() {
-    return cards
-}
-function getClient() {
-    return client
+    users = db.collection("Users")
+    
 }
 
-export { setUpDB, getDB, getCards, getClient }
+function getDB() {
+    return db;
+}
+function getCards() {
+    return cards;
+}
+function getUsers() {
+    return users;
+}
+function getClient() {
+    return client;
+}
+
+export default{ setUpDB, getDB, getCards, getUsers, getClient };
